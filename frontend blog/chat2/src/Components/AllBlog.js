c404c1e4ba14fcab66b8274d28d84811de68a0af
Aayush mishra <zccz2066@gmail.com>
2025-05-18 10:37:57 +0530

    Update AllBlog.js

--- frontend blog/chat2/src/Components/AllBlog.js
+++ frontend blog/chat2/src/Components/AllBlog.js
@@ -1,1 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const AllBlog = () => {
+  const [allBlogs, setAllBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBlogs = async () => {
+      try {
+        const res = await axios.get(`http://localhost:5000/api/blogs`);
+        setAllBlogs(res.data);
+      } catch (err) {
+        console.error("Fetch error:", err.message);
+      }
+    };
+    fetchAllBlogs();
+  }, []);
+
+  return (
+    <div>
+      <h3>📚 All Blogs</h3>
+      <ul>
+        {allBlogs.map((blog) => (
+          <li key={blog._id}>
+            <strong>{blog.title}</strong> - <em>{blog.status}</em>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AllBlog;
 
