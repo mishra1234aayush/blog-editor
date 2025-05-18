1e328534054d39e7a2195c8d78cbfc3f9264c7f6
Aayush mishra <zccz2066@gmail.com>
2025-05-18 10:35:52 +0530

    Update PublishBlog.js

--- frontend blog/chat2/src/Components/PublishBlog.js
+++ frontend blog/chat2/src/Components/PublishBlog.js
@@ -1,1 +1,30 @@
 
+import axios from "axios";
+
+const PublishBlog = ({ blog, blogId }) => {
+  const publisher = async () => {
+    try {
+                   alert("Blog published successfully!");
+      const res = await axios.post("http://localhost:5000/api/blogs/publish", {
+        ...blog,
+        _id: blogId,
+        status: "published",
+      });
+     
+
+      console.log(res);
+    } 
+    
+    catch (err) {
+      alert("Error while publishing");
+    }
+  };
+
+  return (
+    <button className="blog-editor-button" onClick={publisher}>
+      🚀 Publish
+    </button>
+  );
+};
+
+export default PublishBlog;
