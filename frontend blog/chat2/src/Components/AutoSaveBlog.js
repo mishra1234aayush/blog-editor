1846a73d710cbe3bdf71a78dff9d8f0eadb01c2a
Aayush mishra <zccz2066@gmail.com>
2025-05-18 10:37:23 +0530

    Update AutoSaveBlog.js

--- frontend blog/chat2/src/Components/AutoSaveBlog.js
+++ frontend blog/chat2/src/Components/AutoSaveBlog.js
@@ -1,1 +1,32 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+
+const AutoSaveBlog = ({ blog, blogId, setBlogId }) => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      autoSave();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  });
+
+  const autoSave = async () => {
+    try {
+      const res = await axios.post("http://localhost:5000/api/blogs/save-draft", {
+        ...blog,
+        _id: blogId,
+        status: "draft",
+      });
+      setBlogId(res.data._id);
+      console.log("Auto-saved:", new Date().toLocaleTimeString());
+    } catch (err) {
+      console.error("Auto-save error:", err.message);
+    }
+  };
+
+  return null;
+};
+
+export default AutoSaveBlog;
 
