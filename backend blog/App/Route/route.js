37eb14c992679614c0eccb80ca336d2b4c270cd1
Aayush mishra <zccz2066@gmail.com>
2025-05-18 10:53:18 +0530

    Update route.js

--- backend blog/App/Route/route.js
+++ backend blog/App/Route/route.js
@@ -1,6 +1,7 @@
 const controller = require('../Controller/controller');
 
 module.exports = (app)=> {
+    app.get('/',controller.basicInformation);
  app.post('/api/blogs/save-draft',controller.saveOrUpdateDraft);
   app.post('/api/blogs/publish',controller.publishBlog);
  app.get('/api/blogs',controller.getAllBlog);
